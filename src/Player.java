1636b6e99449290dc8cf23209268af2ac4515639
kovatiger <kovalevvvlad981@gmail.com>
2021-12-05 01:29:09 +0300

    Player

---
+++ src/Player.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Player {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void printField(char field[][]) {
+
+    }
+
+    public static void field() {
+        System.out.println("Расставьте ваши корабли");
+    }
+}
